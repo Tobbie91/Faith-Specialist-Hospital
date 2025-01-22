bc64ddef135a69aa2355260934e9e64bc2e1bcef
Tobbie91 <oluwatobiakinrimisi@gmail.com>
2025-01-23 00:27:38 +0100

    m

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,32 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
-//  module.exports = {
-//   plugins: [
-//     `gatsby-plugin-image`,
-//     `gatsby-plugin-sharp`,
-//     `gatsby-transformer-sharp`,
-//     `gatsby-plugin-react-helmet`,
-//     `gatsby-plugin-smoothscroll`,
-//     "gatsby-plugin-postcss",
-//     "gatsby-plugin-mdx",
-//     "gatsby-transformer-remark",
-//     `gatsby-transformer-json`,
-//     {
-//       resolve: `gatsby-source-filesystem`,
-//       options: {
-//         name: `images`,
-//         path: `${__dirname}/src/images`,
-//       },
-//     },
-//   ],
-// };
 module.exports = {
   siteMetadata: {
     title: "Faith Specialist Hospital",
@@ -47,5 +18,20 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-smoothscroll`,
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-remark",
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
   ],
 };
