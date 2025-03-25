28bc92ddcdf8c23878f859a1f9b72ef6ea49c6f9
Tobbie91 <oluwatobiakinrimisi@gmail.com>
2025-03-25 17:02:44 +0100

    fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -18,6 +18,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-sitemap",
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
